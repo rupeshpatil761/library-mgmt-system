282d97288a14aca1dbd26025986c6b2f22a33da1
Rupesh Patil <patilrd861@gmail.com>
2021-07-10 10:59:15 +0530

    gradle config changes in library service

--- library-service/src/test/java/com/library/service/contract/LibraryServiceContractTest.java
+++ library-service/src/test/java/com/library/service/contract/LibraryServiceContractTest.java
@@ -1,23 +1,28 @@
 package com.library.service.contract;
 
 import com.library.service.LibraryServiceApplication;
-import org.junit.Test;
-import static org.hamcrest.Matchers.is;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@RunWith(SpringRunner.class)
+import static org.hamcrest.Matchers.is;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = LibraryServiceApplication.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
-@AutoConfigureStubRunner(stubsMode = StubRunnerProperties.StubsMode.LOCAL, ids = {"com.rupesh.library:book-service"})
+@AutoConfigureStubRunner(stubsMode = StubRunnerProperties.StubsMode.LOCAL, ids = "com.rupesh.library:book-service:+:stubs:9092")
+//@ActiveProfiles("test")
 public class LibraryServiceContractTest {
 
     @Autowired
