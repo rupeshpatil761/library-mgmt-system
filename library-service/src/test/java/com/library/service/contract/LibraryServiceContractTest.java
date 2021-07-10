c10628d35c5c18d53d1874c15df639d1a3dd8842
Rupesh Patil <patilrd861@gmail.com>
2021-07-10 11:37:17 +0530

    local file stubs pointing

--- library-service/src/test/java/com/library/service/contract/LibraryServiceContractTest.java
+++ library-service/src/test/java/com/library/service/contract/LibraryServiceContractTest.java
@@ -8,8 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -19,20 +18,29 @@ import static org.hamcrest.Matchers.is;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = LibraryServiceApplication.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
+// To use this: run "clean build publishToMavenLocal" on producer side.
 @AutoConfigureStubRunner(stubsMode = StubRunnerProperties.StubsMode.LOCAL, ids = "com.rupesh.library:book-service:+:stubs:9092")
-//@ActiveProfiles("test")
+//@AutoConfigureStubRunner( stubsMode = StubRunnerProperties.StubsMode.REMOTE, repositoryRoot = "stubs://file://C:/Users/Rupesh_Patil/Downloads/JPOP%20Workspace/library-mgmt-system/book-service/build/libs/", ids = "com.rupesh.library:book-service")
 public class LibraryServiceContractTest {
+
+    /*@Autowired
+    WebApplicationContext wac;*/
 
     @Autowired
     private MockMvc mockMvc;
 
+    /*@BeforeEach
+    public void before() {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).dispatchOptions(true).build();
+    }*/
+
     @Test
     public void validate_get_book_by_id_contract() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/books/12"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/books/12").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.id", is(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", is(12)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name", is("test thriller book 1")))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.description", is("test thriller book 1 desc")))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.author", is("Author-1")))
